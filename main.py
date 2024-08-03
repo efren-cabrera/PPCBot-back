0957a0c621e9d8f65341e10e772e1aa33bf6887b
Efren Cabrera <efren@cabrera.dev>
2024-08-02 22:34:08 -0700

    Add licence header

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+#  (C) Copyright  2024 DigitalLab.  Confidential computer software.
+#  Valid license from DigitalLab required for possession, use or copying.
+#  Consistent with FAR 12.211 and 12.212, Commercial Computer Software,
+#  Computer Software Documentation, and Technical Data for Commercial Items are
+#  licensed to the U.S. Government under vendor's standard commercial license.
+
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
